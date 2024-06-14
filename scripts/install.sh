db22c553ded26e19c59abf6a263318e7af515707
Prudhvi Kothakonda <kotda@tamu.edu>
2024-06-14 17:18:55 -0500

    running

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,12 +3,15 @@
 sudo apt update
 sudo apt install python3
 sudo apt install python3-pip
+sudo apt install tesseract-ocr
+sudo apt install libtesseract-dev
 python3 --version
 pip3 --version
 
-pip install layoutparser # Install the base layoutparser library with  
-pip install "layoutparser[layoutmodels]" # Install DL layout model toolkit 
-pip install "layoutparser[ocr]" # Install OCR toolkit
-pip install ipykernel # Install the ipykernel for jupyter notebook
-python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
-pip install ipywidgets
+sudo pip install layoutparser # Install the base layoutparser library with  
+sudo pip install "layoutparser[layoutmodels]" # Install DL layout model toolkit 
+sudo pip install "layoutparser[ocr]" # Install OCR toolkit
+sudo pip install ipykernel # Install the ipykernel for jupyter notebook
+sudo python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+sudo pip install ipywidgets
+
